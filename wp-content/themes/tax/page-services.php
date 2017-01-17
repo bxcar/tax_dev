b32fff1f591a0e1d39a3681243fe739149449ec9
bxcar <malanchukdima@mail.ru>
2017-01-17 08:46:57 +0200

    some changes

--- wp-content/themes/tax/page-services.php
+++ wp-content/themes/tax/page-services.php
@@ -10,8 +10,9 @@
 
     <meta charset="utf-8">
 
-    <title>Услуги и цены</title>
-    <meta name="description" content="">
+    <title><?php the_field('services_page_title') ?></title>
+    <meta name="description" content="<?php the_field('services_page_description') ?>">
+    <meta name="keywords" content="<?php the_field('services_page_keywords') ?>">
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -42,7 +43,7 @@
 <!-- Content -->
 <main>
     <section class="top-block top-services">
-        <h1 class="page-title">Услуги и цены</h1>
+        <h1 class="page-title"><?php the_field('services_main_title') ?></h1>
         <div class="breadcrumb">
             <ul>
                 <li><a href="index.html">Главная</a></li>
@@ -51,87 +52,53 @@
         </div>
     </section>
     <section class="services-page">
-        <div class="title">Услуги</div>
+        <div class="title"><?php the_field('services_page_s_title') ?></div>
         <div class="wrap wow fadeInUp" data-wow-duration="1s">
-            <a href="service-vnutr.html" class="item">
-                <div class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/services-1.png" alt="">
-                </div>
-                <p>Оффшорные компании</p>
-            </a>
-            <a href="service-vnutr.html" class="item">
-                <div class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/services-2.png" alt="">
-                </div>
-                <p>Европейские компании</p>
-            </a>
-            <a href="service-vnutr.html" class="item">
-                <div class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/services-3.png" alt="">
-                </div>
-                <p>Компании США и Канады</p>
-            </a>
+            <?php $services = get_field('services_page_s_services');
+            foreach ($services as $service) {
+                ?>
+                <a href="<?=$service['services_page_s_services_single_service_link']?>" class="item">
+                    <div class="img-block">
+                        <img src="<?=$service['services_page_s_services_single_service_img']?>" alt="">
+                    </div>
+                    <p><?=$service['services_page_s_services_single_service_text']?></p>
+                </a>
+                <?php
+            }
+            ?>
         </div>
     </section>
     <section class="footnote wow fadeInUp" data-wow-duration="1s">
-            <p>Информация в этом разделе не является предложением о продаже или предложением в приобретении услуг и предоставленна только в ознокомительных целях. Использование информации в любой юрисдикции, где это запрещено законом, может повлечь за собой преследование по соответствующим статьям существующего законодательства. Консультации предоставляются нерезидентам Украины или выезжающим за рубеж.</p>
+            <p><?php the_field('services_page_s_description') ?></p>
     </section>
     <section class="feadback-form flags-form">
         <div class="form-wrap wow fadeInUp" data-wow-duration="1s">
-            <div class="title-form">Получить консультацию</div>
+            <div class="title-form"><?php the_field('services_page_gc_title') ?></div>
             <form action="">
-                <input type="text" placeholder="Имя">
-                <input type="email" placeholder="Email">
-                <textarea name="" placeholder="Текст"></textarea>
-                <input type="submit" value="Отправить">
+                <input type="text" placeholder="<?php the_field('services_page_gc_placeholder1') ?>">
+                <input type="email" placeholder="<?php the_field('services_page_gc_placeholder2') ?>">
+                <textarea name="" placeholder="<?php the_field('services_page_gc_placeholder3') ?>"></textarea>
+                <input type="submit" value="<?php the_field('services_page_gc_button_text') ?>">
             </form>
         </div>
     </section>
     <section class="facilities-services-block wow fadeInUp" data-wow-duration="1s">
-        <div class="title">Услуги и сервисы</div>
+        <div class="title"><?php the_field('services_page_ss_title') ?></div>
         <div class="wrap">
-            <a href="facilities-services-vnutr.html" class="item">
+            <?php $services_and_facilities = get_field('services_page_ss_services');
+            foreach ($services_and_facilities as $service_and_facility) {
+                ?>
+                <a href="<?=$service_and_facility['services_page_ss_services_single_service_link']?>" class="item">
                 <span class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/f-services-1.png" alt="">
+                    <img src="<?=$service_and_facility['services_page_ss_services_single_service_img']?>" alt="">
                 </span>
-                <span class="text">Регистрация торговых марок <br> и получение штрих кодов</span>
-            </a>
-            <a href="facilities-services-vnutr.html" class="item">
-                <span class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/f-services-2.png" alt="">
-                </span>
-                <span class="text">Получение <br> Сертификата происхождения <br> в Великобритании</span>
-            </a>
-            <a href="facilities-services-vnutr.html" class="item">
-                <span class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/f-services-3.png" alt="">
-                </span>
-                <span class="text">Консультации<br> по счетам Paypal</span>
-            </a>
-            <a href="facilities-services-vnutr.html" class="item">
-                <span class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/f-services-4.png" alt="">
-                </span>
-                <span class="text">Получения<br> номера EORI</span>
-            </a>
-            <a href="facilities-services-vnutr.html" class="item">
-                <span class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/f-services-5.png" alt="">
-                </span>
-                <span class="text">Аккредитация<br> представительства<br> в Украине</span>
-            </a>
-            <a href="#" class="item">
-                <span class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/f-services-6.png" alt="">
-                </span>
-                <span class="text">Помощь специалиста по <br> сервису VAT в Европе</span>
-            </a>
-            <a href="facilities-services-vnutr.html" class="item">
-                <span class="img-block">
-                    <img src="<?php bloginfo('template_url');?>/img/f-services-7.png" alt="">
-                </span>
-                <span class="text">Консультации по счетам<br> Interactive Brokers</span>
-            </a>
+                    <span class="text">
+                        <?=$service_and_facility['services_page_ss_services_single_service_text']?>
+                    </span>
+                </a>
+                <?php
+            }
+            ?>
         </div>
     </section>
 </main>
